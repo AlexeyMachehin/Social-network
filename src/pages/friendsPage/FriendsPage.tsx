e90624efd3e768ab5e99295e4f77c4bff96d8209
AlexeyMachehin <99137228+AlexeyMachehin@users.noreply.github.com>
2023-05-06 04:14:46 +0500

    create newsPage, friendsPage

--- src/pages/friendsPage/FriendsPage.tsx
+++ src/pages/friendsPage/FriendsPage.tsx
@@ -1,3 +1,12 @@
+import classes from './friendsPage.module.css';
+
 export default function FriendsPage() {
-    return <><div>friends</div></>;
-  }
+  return (
+    <>
+      <h2 style={{ textAlign: 'center', marginBottom: '10px' }}>
+        Friends list
+      </h2>
+      <div className={`container ${classes.wrapper}`}></div>
+    </>
+  );
+}
--- src/pages/newsPage/NewsPage.tsx
+++ src/pages/newsPage/NewsPage.tsx
@@ -1,7 +1,34 @@
-export default function NewsPage() {
+import Post from '../../components/post/Post';
+import classes from './newsPage.module.css';
+
+export default function FriendsPage() {
+  const posts = true;
+
   return (
     <>
-      <div>news</div>
+      <h2 style={{ textAlign: 'center', marginBottom: '10px' }}>News</h2>
+      <div className={`container ${classes.wrapper}`}>
+        {posts ? (
+          <div className={classes.postsList}>
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+          </div>
+        ) : (
+          <div>No posts</div>
+        )}
+      </div>
     </>
   );
 }
