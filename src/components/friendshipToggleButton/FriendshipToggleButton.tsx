4ae70dcf957c37572dc9dcb39008edb7b36af453
AlexeyMachehin <99137228+AlexeyMachehin@users.noreply.github.com>
2023-05-06 01:41:46 +0500

    df

--- src/components/friendshipToggleButton/FriendshipToggleButton.tsx
+++ src/components/friendshipToggleButton/FriendshipToggleButton.tsx
@@ -2,7 +2,7 @@ import classes from './friendshipToggleButton.module.css';
 
 export default function FriendshipToggleButton() {
   const isFriend = true;
-  
+
   return (
     <div className={classes.wrapper}>
       {isFriend ? 'Remove from friends' : 'Add to friends'}
