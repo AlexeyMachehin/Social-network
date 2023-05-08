3624064be55b7576ebe03df2eb9eced6f06c6467
AlexeyMachehin <99137228+AlexeyMachehin@users.noreply.github.com>
2023-05-09 00:18:19 +0500

    fix error with loader

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -4,28 +4,6 @@ import { getAuth, onAuthStateChanged } from '@firebase/auth';
 import { setUser } from '@/store/slices/userSlice';
 import { setIsLoaderOff, setIsLoaderOn } from '@/store/slices/loaderSlice';
 
-// export function useAuth() {
-//   const dispatch = useAppDispatch();
-//   const auth = getAuth();
-
-//   useEffect(() => {
-//     const unsubscribe = onAuthStateChanged(auth, (user: any) => {
-//       if (user) {
-//         dispatch(
-//           setUser({
-//             email: user.email,
-//             id: user.uid,
-//             photoURL: user.photoURL,
-//             token: user.accessToken,
-//           }),
-//         );
-//       }
-//     });
-
-//     return unsubscribe;
-//   }, [auth]);
-// }
-
 export function useAuth() {
   const dispatch = useAppDispatch();
   const auth = getAuth();
@@ -42,8 +20,8 @@ export function useAuth() {
             token: user.accessToken,
           }),
         );
-        dispatch(setIsLoaderOff());
       }
+      dispatch(setIsLoaderOff());
     });
 
     return unsubscribe;
