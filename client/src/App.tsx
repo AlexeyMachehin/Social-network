0b8f91eb799ca6cd7e6f682eeff19ee0e13de230
AlexeyMachehin <99137228+AlexeyMachehin@users.noreply.github.com>
2023-06-27 18:42:32 +0500

    create client package, update eslint settings

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,11 +15,6 @@ import { userService } from './services/userService';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import './styles/App.css';
 
-
-ggg()
- function ggg(){
-
- }
 const theme = createTheme();
 
 function App() {
