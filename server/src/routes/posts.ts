cdb137ea64c52ab4e7164aaf1de8a912ec7fa8fb
AlexeyMachehin <99137228+AlexeyMachehin@users.noreply.github.com>
2023-07-02 01:05:46 +0500

    fix error

--- server/src/routes/posts.ts
+++
@@ -1,7 +0,0 @@
-import { getPosts, createPost } from '@/controllers/postsControllers';
-import { Router } from 'express';
-
-export const postsRouter = Router();
-
-postsRouter.get('/:userId', getPosts);
-postsRouter.post('/:userId', createPost);
